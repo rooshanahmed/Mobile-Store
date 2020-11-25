8491f0b34b0a05cfa42d7e139641b757dcedc978
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-11-26 00:18:43 +0500

    App Updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div>
       <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,48 +1,83 @@
 import React from "react";
-import { AppBar, Button, makeStyles, Toolbar, Typography } from "@material-ui/core";
+import {
+  AppBar,
+  Button,
+  makeStyles,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import { useNavigate } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
-    appbar: {
-        background: 'none',
-        // width: '80%',
-        // margin: '0 auto',
-        // display: 'flex',
-    },
-    text: {
-        fontFamily: 'Ubuntu',
-        color: 'black',
-        fontSize: '20px',
-    },
-    btn: {
-        display: 'flex',
-        marginLeft: 'auto',
-        justifyContent: 'space-between',
-    },
-    appBarWrapper: {
-        width: '80%',
-        margin: '0 auto',
-    },
-    root: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexGrow: '1',
-    },
+  appbar: {
+    background: "none",
+    // width: '80%',
+    // margin: '0 auto',
+    // display: 'flex',
+  },
+  text: {
+    fontFamily: "Ubuntu",
+    color: "black",
+    fontSize: "24px",
+  },
+  btn: {
+    display: "flex",
+    marginLeft: "auto",
+    justifyContent: "space-between",
+  },
+  appBarWrapper: {
+    width: "80%",
+    margin: "0 auto",
+  },
+  root: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexGrow: "1",
+  },
+  button: {
+    margin: "14px",
+  },
 }));
 
 const Header = () => {
   const classes = useStyles();
+  const navigate = useNavigate();
 
   return (
     <div className={classes.root}>
       <AppBar className={classes.appbar} elevation={0}>
         <Toolbar className={classes.appBarWrapper}>
-            <Typography className={classes.text}>Apple Store</Typography>
-            <div className={classes.btn}>
-              <Button disableTouchRipple>Home</Button>
-              <Button disableTouchRipple>Products</Button>
-              <Button disableTouchRipple>About Us</Button>
-            </div>
+          <Typography className={classes.text}>Apple Store</Typography>
+          <div className={classes.btn}>
+            <Button
+              disableTouchRipple
+              className={classes.button}
+              onClick={() => {
+                navigate("/");
+              }}
+            >
+              Home
+            </Button>
+            <Button
+              disableTouchRipple
+              className={classes.button}
+              onClick={() => {
+                navigate("products");
+              }}
+            >
+              Products
+            </Button>
+            <Button
+              disableTouchRipple
+              className={classes.button}
+              onClick={() => {
+                navigate("about");
+              }}
+            >
+              About Us
+            </Button>
+          </div>
         </Toolbar>
       </AppBar>
     </div>
